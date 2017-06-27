3d41f7d7ca806239494e46ff90002722cbde75f8
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-27 12:54:50 +0200

    refactor

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -1,9 +1,7 @@
-import DS from 'ember-data';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr'
 
 export default Model.extend({
   amount: attr('number'),
-  createdAt: attr('date', { defaultValue() { return new Date(); }
-  })
+  createdAt: attr('date', { defaultValue() { return new Date(); } })
 });
--- app/services/input-validator.js
+++ app/services/input-validator.js
@@ -6,16 +6,6 @@ export default Service.extend({
   store: service(),
   flashMessages: service(),
 
-  // add(record) {
-  //   let validate = this.get('validate');
-  //   if(validate(record)) {
-  //     this.get('store').createRecord('transaction', { amount: record }).save()
-  //     .then(() => { this.get('flashMessages').success("record created") })
-  //   } else {
-  //     this.get('flashMessages').success('Amount should higher than 1 and higher zero');
-  //   }
-  // },
-
   validate(record) {
     return record > 0;
   },
