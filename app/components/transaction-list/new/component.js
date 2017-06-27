1e4158a3da227a1fd89f69fa8a5f0ff5cfd205ca
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-27 09:24:39 +0200

    create new transaction

--- app/components/transaction-list/new/component.js
+++ app/components/transaction-list/new/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  amountValue: '',
+
+  actions: {
+    newTransaction() {
+      let amount = this.get('amountValue');
+      this.get('store').createRecord('transaction', {amount})
+    }
+  }
 });
